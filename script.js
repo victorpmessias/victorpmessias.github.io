ead04d48cc62a1e1da4e5182de7c37a0081319f3
victorpmessias <victor.p.messias@gmail.com>
2021-12-30 19:36:06 -0300

    Implementação do efeito de scroll em link interno com JS

---
+++ script.js
@@ -0,0 +1,2 @@
+function initScrollSuave(){const linksInternos=document.querySelectorAll('.js-menu a[href^="#"]');function scrollSuave(event){event.preventDefault();const href=event.currentTarget.getAttribute('href');const section=document.querySelector(href);section.scrollIntoView({behavior:'smooth',block:'start',})}
+linksInternos.forEach((link)=>{link.addEventListener('click',scrollSuave)})}initScrollSuave()
